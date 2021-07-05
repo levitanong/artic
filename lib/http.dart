95ccfb0b8be928118931cb6ce5f12689fcb94606
Levi Tan Ong <levi.ong@gmail.com>
2021-07-05 20:29:03 +0800

    use dio instead of dart-http

--- lib/http.dart
+++ lib/http.dart
@@ -1,20 +1,22 @@
 import 'dart:async';
 import 'dart:convert';
+import 'package:dio/dio.dart';
 import 'package:http/http.dart' as http;
 import 'adapters.dart';
 
 Future<ArticArtworkPayload> fetchArtworks() async {
-  final url = Uri.parse(
-      'https://api.artic.edu/api/v1/artworks?page=1&limit=10&fields=id,api_link,title,thumbnail,artist_display,date_display,medium_display,dimensions,main_reference_number');
+  final url =
+      'https://api.artic.edu/api/v1/artworks?page=1&limit=10&fields=id,api_link,title,thumbnail,artist_display,date_display,medium_display,dimensions,main_reference_number';
   try {
-    final http.Response response = await http
-        .get(url)
-        .timeout(Duration(milliseconds: 5000), onTimeout: () {
-      throw TimeoutException("http request timed out");
-    });
+    // final http.Response response = await http
+    //     .get(url)
+    //     .timeout(Duration(milliseconds: 5000), onTimeout: () {
+    //   throw TimeoutException("http request timed out");
+    // });
+    final response = await Dio().get(url);
     switch (response.statusCode) {
       case 200:
-        final body = jsonDecode(response.body);
+        final body = response.data; //jsonDecode(response.data);
         final articPayload = ArticArtworkPayload.fromJson(body);
         return articPayload;
       default:
@@ -26,13 +28,13 @@ Future<ArticArtworkPayload> fetchArtworks() async {
 }
 
 Future<ArticArtwork> fetchArtwork(String id) async {
-  final url = Uri.parse("https://api.artic.edu/api/v1/artworks/$id");
-  final response = await http.get(url);
+  final url = "https://api.artic.edu/api/v1/artworks/$id";
+  final response = await Dio().get(url);
 
   // Using switch-case because we might want to handle other statusCodes differently
   switch (response.statusCode) {
     case 200:
-      final body = jsonDecode(response.body);
+      final body = response.data;
       final articArtwork = ArticArtwork.fromJson(body['data']);
       return articArtwork;
     case 404:
