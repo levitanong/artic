d5f3fd8fa45058ead7bd835d267b04bbaba06e4d
Levi Tan Ong <levi.ong@gmail.com>
2021-07-07 12:30:56 +0800

    try switching back to dart http

--- lib/http.dart
+++ lib/http.dart
@@ -1,21 +1,23 @@
 import 'dart:async';
 import 'dart:convert';
-import 'package:dio/dio.dart';
+import 'dart:html';
+import 'package:http/http.dart';
+
 import 'adapters.dart';
 
 Future<ArticArtworkPayload> fetchArtworks() async {
-  final url =
-      'https://api.artic.edu/api/v1/artworks?page=1&limit=10&fields=id,api_link,title,thumbnail,artist_display,date_display,medium_display,dimensions,main_reference_number';
+  final fields =
+      'id,api_link,title,thumbnail,artist_display,date_display,medium_display,dimensions,main_reference_number';
+  final url = Uri(
+      scheme: 'https',
+      host: 'api.artic.edu',
+      path: '/api/v1/artworks',
+      queryParameters: {'page': '1', 'limit': '10', 'fields': fields});
   try {
-    // final http.Response response = await http
-    //     .get(url)
-    //     .timeout(Duration(milliseconds: 5000), onTimeout: () {
-    //   throw TimeoutException("http request timed out");
-    // });
-    final response = await Dio().get(url);
+    final Response response = await get(url);
     switch (response.statusCode) {
       case 200:
-        final body = response.data; //jsonDecode(response.data);
+        final body = jsonDecode(response.body);
         final articPayload = ArticArtworkPayload.fromJson(body);
         return articPayload;
       default:
@@ -27,13 +29,13 @@ Future<ArticArtworkPayload> fetchArtworks() async {
 }
 
 Future<ArticArtwork> fetchArtwork(String id) async {
-  final url = "https://api.artic.edu/api/v1/artworks/$id";
-  final response = await Dio().get(url);
+  final url = Uri.parse("https://api.artic.edu/api/v1/artworks/$id");
+  final Response response = await get(url);
 
   // Using switch-case because we might want to handle other statusCodes differently
   switch (response.statusCode) {
     case 200:
-      final body = response.data;
+      final body = jsonDecode(response.body);
       final articArtwork = ArticArtwork.fromJson(body['data']);
       return articArtwork;
     case 404:
--- lib/stores/artworks.dart
+++ lib/stores/artworks.dart
@@ -2,25 +2,24 @@ import 'dart:async';
 
 import 'package:artic/adapters.dart';
 import 'package:artic/http.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 
 class ArtworksStore extends ChangeNotifier {
   ArticArtworkPayload? _payload;
   bool _hasError = false;
-  DioError? _exception;
+  Exception? _exception;
 
   /// We basically want to prevent payload to be settable from outside.
   ArticArtworkPayload? get payload => _payload;
   bool get hasError => _hasError;
-  DioError? get exception => _exception;
+  Exception? get exception => _exception;
 
   Future<void> fetch() async {
     /// fetchArtworks already handles conversion
     /// to the right data types so we can just use it.
     try {
       _payload = await fetchArtworks();
-    } on DioError catch (d) {
+    } on Exception catch (d) {
       _hasError = true;
       _exception = d;
     }
