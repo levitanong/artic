6c2b7e766ae350afc117d0619b18d6ad1e1c056e
Levi Tan Ong <levi.ong@gmail.com>
2021-07-07 11:40:51 +0800

    back to https

--- lib/http.dart
+++ lib/http.dart
@@ -5,7 +5,7 @@ import 'adapters.dart';
 
 Future<ArticArtworkPayload> fetchArtworks() async {
   final url =
-      'http://api.artic.edu/api/v1/artworks?page=1&limit=10&fields=id,api_link,title,thumbnail,artist_display,date_display,medium_display,dimensions,main_reference_number';
+      'https://api.artic.edu/api/v1/artworks?page=1&limit=10&fields=id,api_link,title,thumbnail,artist_display,date_display,medium_display,dimensions,main_reference_number';
   try {
     // final http.Response response = await http
     //     .get(url)
@@ -27,7 +27,7 @@ Future<ArticArtworkPayload> fetchArtworks() async {
 }
 
 Future<ArticArtwork> fetchArtwork(String id) async {
-  final url = "http://api.artic.edu/api/v1/artworks/$id";
+  final url = "https://api.artic.edu/api/v1/artworks/$id";
   final response = await Dio().get(url);
 
   // Using switch-case because we might want to handle other statusCodes differently
